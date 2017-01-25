b16bdd454334c5acd1064eca153d6a1a3c03ce5e
SuperNami <supernami@tutamail.com>
2017-01-25 12:44:03 +0530

    Updates

--- script.sh
+++ script.sh
@@ -17,31 +17,45 @@ service apache2 restart
 service apache2 stop
 
 apt-get install gcc make autoconf libc-dev pkg-config -y
-pecl install apcu -y
 
 bash -c "echo extension=apcu.so > /etc/php/7.0/cli/conf.d/20-apcu.ini"
 bash -c "echo extension=apcu.so > /etc/php/7.0/fpm/conf.d/20-apcu.ini"
 bash -c "echo extension=apcu.so > /etc/php/7.0/apache2/conf.d/20-apcu.ini"
 
+sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
+sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.0/fpm/pool.d/www.conf
+
 sleep 5
 #wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/fastcgi_params -O /etc/nginx/fastcgi_params
-wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal8.conf -O /etc/nginx/sites-available/drupal8.conf
+
+wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal1.conf -O /etc/nginx/sites-available/drupal.conf
+#wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal2.conf -O /etc/nginx/sites-available/drupal.conf
 ln -s /etc/nginx/sites-available/drupal8.conf /etc/nginx/sites-enabled/drupal8.conf
 
+wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/nginx.conf -O /etc/nginx/nginx.conf
+
+# helper utility
 wget https://raw.githubusercontent.com/GabrielGil/c9-lemp/master/lemp -O /usr/bin/lemp
 chmod 755 /usr/bin/lemp
 
 # update Composer
 composer self-update
 
+# start
+sleep 5
 service nginx start
 service nginx reload
 service php7.0-fpm start
 mysql-ctl start
 
+# status
 sleep 5
 service nginx status
 service php7.0-fpm status
 service mysql status
+
+# this command stops the script with user input where you have to hit [enter] key
+pecl install apcu -y
 
 # curl -L https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/script.sh | bash
