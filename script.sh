febd2d292718b2a437c2c0247de5822272257785
SuperNami <supernami@tutamail.com>
2017-01-25 11:34:51 +0530

    Fixes

--- script.sh
+++ script.sh
@@ -11,6 +11,8 @@ apt-get install -y php7.0 php7.0-fpm php7.0-curl php7.0-dev php7.0-gd php7.0-int
 apt-get install libapache2-mod-php7.0 -y
 a2dismod php5
 a2enmod php7.0
+a2enconf php7.0-fpm
+a2enmod proxy_fcgi setenvif
 service apache2 restart
 service apache2 stop
 
@@ -21,6 +23,7 @@ bash -c "echo extension=apcu.so > /etc/php/7.0/cli/conf.d/20-apcu.ini"
 bash -c "echo extension=apcu.so > /etc/php/7.0/fpm/conf.d/20-apcu.ini"
 bash -c "echo extension=apcu.so > /etc/php/7.0/apache2/conf.d/20-apcu.ini"
 
+sleep 5
 #wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/fastcgi_params -O /etc/nginx/fastcgi_params
 wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal8.conf -O /etc/nginx/sites-available/drupal8.conf
 ln -s /etc/nginx/sites-available/drupal8.conf /etc/nginx/sites-enabled/drupal8.conf
