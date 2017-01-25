a3b82af2d5a3dc3dd92ad26c3049b68ad3bcfac5
SuperNami <supernami@tutamail.com>
2017-01-25 10:07:59 +0530

    Add configuration

--- script.sh
+++ script.sh
@@ -3,10 +3,10 @@
 add-apt-repository ppa:ondrej/php -y
 apt-get update -y
 
-apt-get install php7.0-curl php7.0-dev php7.0-gd php7.0-intl \
-                php7.0-mcrypt php7.0-json php7.0-mysql php7.0-sqlite \
-                php7.0-opcache php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-xml \
-                php7.0-dom php7.0-mbstring php7.0-zip
+apt-get install -y php7.0-curl php7.0-dev php7.0-gd php7.0-intl \
+                   php7.0-mcrypt php7.0-json php7.0-mysql php7.0-sqlite \
+                   php7.0-opcache php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-xml \
+                   php7.0-dom php7.0-mbstring php7.0-zip
 
 apt-get install libapache2-mod-php7.0 -y
 a2dismod php5
@@ -21,6 +21,6 @@ bash -c "echo extension=apcu.so > /etc/php/7.0/apache2/conf.d/20-apcu.ini"
 
 service apache2 restart
 
-wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/fastcgi_params
-wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/nginx.conf
-wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/drupal8.conf
+#wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/fastcgi_params
+wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/nginx.conf -O /etc/nginx/nginx.conf
+wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/drupal8.conf -O /etc/nginx/sites-available/drupal8.conf
