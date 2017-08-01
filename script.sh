7fc5871d4e905241c753c2aa317c6daf8c052f24
SuperNami <supernami@tutamail.com>
2017-08-01 09:22:19 +0200

    Updates

--- script.sh
+++ script.sh
@@ -3,35 +3,34 @@
 add-apt-repository ppa:ondrej/php -y
 apt-get update -y
 
-apt-get install -y php7.0 php7.0-fpm php7.0-curl php7.0-dev php7.0-gd php7.0-intl \
-                   php7.0-mcrypt php7.0-json php7.0-mysql php7.0-sqlite \
-                   php7.0-opcache php7.0-bcmath php7.0-mbstring php7.0-soap php7.0-xml \
-                   php7.0-dom php7.0-mbstring php7.0-zip
+apt-get install -y php7.1 php7.1-fpm php7.1-curl php7.1-dev php7.1-gd php7.1-intl \
+                   php7.1-mcrypt php7.1-json php7.1-mysql php7.1-sqlite \
+                   php7.1-opcache php7.1-bcmath php7.1-mbstring php7.1-soap php7.1-xml \
+                   php7.1-dom php7.1-mbstring php7.1-zip
 
-apt-get install libapache2-mod-php7.0 -y
+apt-get install libapache2-mod-php7.1 -y
 a2dismod php5
-a2enmod php7.0
-a2enconf php7.0-fpm
+a2enmod php7.1
+a2enconf php7.1-fpm
 a2enmod proxy_fcgi setenvif
 service apache2 restart
 service apache2 stop
 
 apt-get install gcc make autoconf libc-dev pkg-config -y
 
-bash -c "echo extension=apcu.so > /etc/php/7.0/cli/conf.d/20-apcu.ini"
-bash -c "echo extension=apcu.so > /etc/php/7.0/fpm/conf.d/20-apcu.ini"
-bash -c "echo extension=apcu.so > /etc/php/7.0/apache2/conf.d/20-apcu.ini"
+bash -c "echo extension=apcu.so > /etc/php/7.1/cli/conf.d/20-apcu.ini"
+bash -c "echo extension=apcu.so > /etc/php/7.1/fpm/conf.d/20-apcu.ini"
+bash -c "echo extension=apcu.so > /etc/php/7.1/apache2/conf.d/20-apcu.ini"
 
-sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/listen.owner = www-data/listen.owner = ubuntu/g' /etc/php/7.0/fpm/pool.d/www.conf
-sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.0/fpm/pool.d/www.conf
+sed -i 's/user = www-data/user = ubuntu/g' /etc/php/7.1/fpm/pool.d/www.conf
+sed -i 's/group = www-data/group = ubuntu/g' /etc/php/7.1/fpm/pool.d/www.conf
+sed -i 's/listen.owner = www-data/listen.owner = ubuntu/g' /etc/php/7.1/fpm/pool.d/www.conf
+sed -i 's/pm = dynamic/pm = ondemand/g' /etc/php/7.1/fpm/pool.d/www.conf
 
 sleep 5
 #wget https://raw.githubusercontent.com/wodby/drupal-nginx/master/1.10/fastcgi_params -O /etc/nginx/fastcgi_params
 
-wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal1.conf -O /etc/nginx/sites-available/drupal.conf
-#wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal2.conf -O /etc/nginx/sites-available/drupal.conf
+wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/drupal.conf -O /etc/nginx/sites-available/drupal.conf
 ln -s /etc/nginx/sites-available/drupal.conf /etc/nginx/sites-enabled/drupal.conf
 
 wget https://raw.githubusercontent.com/SuperNami/c9-nginx-php7/master/nginx.conf -O /etc/nginx/nginx.conf
@@ -47,13 +46,13 @@ composer self-update
 sleep 5
 service nginx start
 service nginx reload
-service php7.0-fpm start
+service php7.1-fpm start
 mysql-ctl start
 
 # status
 sleep 5
 service nginx status
-service php7.0-fpm status
+service php7.1-fpm status
 service mysql status
 
 # this command stops the script with user input where you have to hit [enter] key
